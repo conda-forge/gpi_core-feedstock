472ceb275d623c00ebe8ae95d9bb643cbb53f0b3
Daniel Borup <borup.daniel@mayo.edu>
2019-07-31 10:24:39 -0500

    Pin numpy to 1.11 at build & try to fix test

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -2,7 +2,7 @@
 
 if [ "$(uname)" == "Darwin" ]; then
     OUTPUT=`gpi --nogui repo_contents/testNets/TestNetwork.net | grep "gpi.canvasGraph:384"`
-    if [-z "$OUTPUT" ]; then
+    if [ -z "$OUTPUT" ]; then
         echo "Test Network Failed! Output was not successful completion of network" 1>&2
         exit 1
     else

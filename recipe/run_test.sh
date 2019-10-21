7c741d1403318fe4a38d9332b0a094144f3d11f0
Daniel Borup <borup.daniel@mayo.edu>
2019-10-21 11:10:29 -0500

    Restore linux test and bump build number

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -11,9 +11,14 @@ if [ "$(uname)" == "Darwin" ]; then
 fi
 
 if [ "$(uname)" == "Linux" ]; then
-    echo "Linux Test not currently available."
-#    export DISPLAY=localhost:1.0
-#    OUTPUT_1=`Display=localhost:1.0 xvfb-run -a bash -c "gpi --nogui repo_contents/testNets/TestNetwork_NoMatplotlib.net"`
-#    OUTPUT=`echo $OUTPUT_1 | grep "gpi.canvasGraph:384"`
-#    echo "Ran Linux Test"
+    export DISPLAY=localhost:1.0
+    OUTPUT_1=`Display=localhost:1.0 xvfb-run -a bash -c "gpi --nogui repo_contents/testNets/TestNetwork_NoMatplotlib.net"`
+    OUTPUT=`echo $OUTPUT_1 | grep "gpi.canvasGraph:384"`
+    echo "Ran Linux Test"
+    if [ -z "$OUTPUT" ]; then
+        echo "Test Network Failed! Output was not successful completion of network" 1>&2
+        exit 1
+    else
+        echo "Test Network Executed Successfully!"
+    fi
 fi
